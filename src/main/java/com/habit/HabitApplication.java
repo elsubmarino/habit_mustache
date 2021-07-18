e1e8521a61460fb16f68314d45d556e6125777b2
jisungyeah@gmail.com <zxc06733>
2021-07-18 23:31:31 +0900

    first commit

---
+++ src/main/java/com/habit/HabitApplication.java
@@ -0,0 +1,14 @@
+package com.habit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class HabitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitApplication.class, args);
+    }
+
+}
